9d02f10ee8c8f58ca1c1ea0da2e163a05e76b1f0
TKNOWNUEU <t3ddyadikusuma@student.esaunggul.ac.id>
2022-11-08 01:16:05 +0700

    UTS no 4

---
+++ UTS/jawaban4.py
@@ -0,0 +1,5 @@
+Provinsi_Kalimantan = ["Kalimantan", "Sumatra", "Sulawesi",]
+Lokasi = ["West", "North","South"]
+for x in Provinsi_Kalimantan:
+            for y in Lokasi:
+                print(x,y)
