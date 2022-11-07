7bfb5f69c7344dd341637a162ca769a11021416c
TKNOWNUEU <t3ddyadikusuma@student.esaunggul.ac.id>
2022-11-08 01:15:09 +0700

    UTS no 2

---
+++ UTS/Jawaban2.py
@@ -0,0 +1,7 @@
+#input alas dan tinggi
+alas = int(input("Masukkan alas: "))
+tinggi = int(input("Masukkan tinggi: "))
+#hitung luas segitiga
+luas = alas * tinggi / 2
+#tampilkan luas segitiga
+print("Luas segitiga adalah: ", luas)
