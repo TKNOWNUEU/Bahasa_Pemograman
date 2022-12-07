7cf90aacdd137746e9192efcae1fd502c7999f34
TKNOWNUEU <t3ddyadikusuma@student.esaunggul.ac.id>
2022-12-07 18:31:11 +0700

    grafik

---
+++ meet_10/grafik.py
@@ -0,0 +1,9 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+xpoints = np.array([2,2,1,2,4,5,4,2,1,2,2,3,4,5,6,6,7,7.3,7.3,7,6,6,6,5,4,3,2])
+ypoints = np.array([0,3,4,5,5,4,3,3,4,5,6,7,7,7,6,5,5,4.0,2.9,2,2,5,0,0,2,0,0])
+plt.title("AMONG US")
+plt.plot(xpoints, ypoints)
+plt.show()
+# %%
---
+++ meet_10/readme.md
@@ -0,0 +1,3 @@
+Nama Teddy Adi Kusuma
+NIM 202010801149
+Matplotlib adalah liblary python untuk memvisualisasikan data (plot) garis naik turun sesuai titik nilai x dan y 
